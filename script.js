26d1dfc2960c66f26af28ea864b605b8d9868c96
Sarah <stahar06@gmail.com>
2022-02-10 15:16:06 +0100

    alert modif

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-
 let body = document.querySelector("body");
 let perso = document.getElementById("perso");
 let rat = document.getElementById("rat");
+let alertgo = document.getElementById("alertGO");
+alertgo.style.display="none"; 
+let alertGG = document.getElementById("alertGG");
+alertGG.style.display = "none";
 
 body.classList.add("bganimation");
 
+//score
 let score = 0;
 const X = Math.ceil((100*1000)/50);
 let scoreElem = document.getElementById("score");
@@ -13,9 +17,22 @@ function count(){
   if(score<50){
     setTimeout(count, X);
   }
+  if (score == 50) {
+
+    rat.style.animation = "none";
+    rat.style.display = "none";
+    body.classList.remove("bganimation");
+
+    let pseudogg = prompt("Choisir un pseudo");
+    alertgo.style.display="none"; 
+    let tempsgg = new joueur(pseudogg, score);
+    tempsgg.gg(pseudogg);
+  
+  }
 }
 setTimeout(count, X);
 
+//acceleration rat
 function level(){
   if (score>= 5 && score<= 10) {
     rat.style.animationDuration="3.5s";
@@ -43,7 +60,7 @@ function level(){
   } 
 };
 
-
+//animation jump
 document.addEventListener('keydown', function(jump){
   if (jump.code === 'Space'){
     perso.classList.add("animate");
@@ -55,40 +72,52 @@ document.addEventListener('keydown', function(jump){
     },500);
   }
 });
-let win = setInterval(function(){
-  if (score == 50) {
-    rat.style.animation = "none";
-    rat.style.display = "none";
-    body.classList.remove("bganimation");
-    alert("YOU WIN !")
+ 
+//personnalisation du joueur
+class joueur{
+  constructor(nom,score){
+    this.nom = nom;
+    this.score = score;
   }
-},10);
 
-// class pseudo{
-//   constructor(nom,score){
-//     this.nom = nom;
-//     this.score = score;
-//   }
-//   perdu(){
-//     document.write("Ton score est de " + score);
-//     // document.write("Le meilleur score est de " + scoreavant);
-//   }
-// }
+  perdu(player){
+    alertgo.style.display ="block";
+    const node = document.createElement("p");
+    const textnode = document.createTextNode(player + " ton score est de " + score + "  Sale noob !!!");
+    node.appendChild(textnode);
+    document.getElementById("loose").appendChild(node);
+  }
 
+  gg(player){
+    alertGG.style.display ="block";
+    const nodegg = document.createElement("p");
+    const textnodegg = document.createTextNode(player + " ton score est de " + score + "  Bravo!!! (personne croyait en toi)");
+    nodegg.appendChild(textnodegg);
+    document.getElementById("gagner").appendChild(nodegg);
+
+  }
+}
+
+//perdu
 let dead = setInterval(function(){
 let persoTop = parseInt(window.getComputedStyle(perso).getPropertyValue("top"));
 let ratLeft = parseInt(window.getComputedStyle(rat).getPropertyValue("left"));
 if (ratLeft < 400 && ratLeft > 350 && persoTop >= 455) {
-  
+window.clearTimeout(count, X);
   rat.style.animation = "none";
   rat.style.display = "none";
   perso.src = `img/dead.png`;
   body.classList.remove("bganimation");
 
-  // let choix = prompt("Choisir un pseudo");
-  // choix.perdu();
-  alert("GAME OVER!")
+//recup score et nom joueur
+  let pseudo = prompt("Choisir un pseudo");
+  let tempsperdu = new joueur(pseudo, score);
+  tempsperdu.perdu(pseudo);
 
-  document.location.reload(false)
 }
 }, 10);
+
+
+
+
+ 
